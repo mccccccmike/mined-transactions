7aa824197dde33173ffbd54b29522eb728800f6e
xmerzi <xmerzi@sina.cn>
2023-03-07 10:06:49 +0800

    fix checking type issue

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const Web3 = require("web3");
-const web3 = new Web3("wss://eth-mainnet.g.alchemy.com/v2/Q5sH2zvE_fu5H1XI4RC09iQXlSHIbJPS");
+var options = {
+  reconnect: {
+      auto: true,
+      delay: 5000, // ms
+      maxAttempts: 5,
+      onTimeout: false
+  }
+};
+const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://eth-mainnet.g.alchemy.com/v2/Q5sH2zvE_fu5H1XI4RC09iQXlSHIbJPS", options));
 
 const abi = [{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
 
@@ -28,34 +36,45 @@ async function saveNewContract(name, type, balance, gas, gasUsed, gasPrice, tran
 }
 
 web3.eth.subscribe("newBlockHeaders", async (error, block) => {
-  const transactions = (await web3.eth.getBlock(block.number,true)).transactions.filter(transaction => transaction.to === null);
-  // console.log(transactions);
-  const recipients = await Promise.all(transactions.map(transaction => web3.eth.getTransactionReceipt(transaction.hash)));
-  const balances = await Promise.all(recipients.map(recipient => web3.eth.getBalance(recipient.from)));
-  // console.log(recipients);
-  recipients.forEach((recipient, index) => {
-    const balance = web3.utils.fromWei('' + balances[index], 'ether');
-    const gas = transactions[index].gas;
-    const gasUsed = recipients[index].gasUsed;
-    const gasPrice = web3.utils.fromWei(transactions[index].gasPrice, 'gwei');
-    const transactionFee = web3.utils.fromWei('' + (gasUsed * transactions[index].gasPrice), 'ether');
-    const logs = recipient.logs;
-    let type = 'NFT';
-    for (const log of logs) {
-      if (log.topics[0] == '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' && log.topics.length == 3) {
-        // erc20
-        type = 'ERC20';
-        break;                                                                                                                                                                                                  
-      } else {
-        // consider everything else is nft contract creation 
-      }
+  if (!error) {
+    try {
+      const transactions = (await web3.eth.getBlock(block.number,true)).transactions.filter(transaction => transaction.to === null);
+      // console.log(transactions);
+      const recipients = await Promise.all(transactions.map(transaction => web3.eth.getTransactionReceipt(transaction.hash)));
+      const balances = await Promise.all(recipients.map(recipient => web3.eth.getBalance(recipient.from)));
+      // console.log(recipients);
+      recipients.forEach((recipient, index) => {
+        const balance = web3.utils.fromWei('' + balances[index], 'ether');
+        const gas = transactions[index].gas;
+        const gasUsed = recipients[index].gasUsed;
+        const gasPrice = web3.utils.fromWei(transactions[index].gasPrice, 'gwei');
+        const transactionFee = web3.utils.fromWei('' + (gasUsed * transactions[index].gasPrice), 'ether');
+        const logs = recipient.logs;
+        let type = null;
+        for (const log of logs) {
+          if (log.topics[0] == '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' && log.topics.length == 3) {
+            // erc20
+            type = 'ERC20';
+            break;                                                                                                                                                                                                  
+          } else if(log.topics[0] == '0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0' && log.topics.length == 3) {
+            // consider everything else is nft contract creation 
+            type = 'NFT';
+          }
+        }
+        
+        if (type) {
+          const erc20Contract = new web3.eth.Contract(abi, recipient.contractAddress);
+          erc20Contract.methods.name().call().then(function(error, name) {
+            // console.log('name = ' + name);
+            erc20Contract.methods.symbol().call((error, symbol) => {
+              // console.log('symbol = ' + symbol);
+              saveNewContract(name + '-' + symbol || '', type, balance, gas, gasUsed, gasPrice, transactionFee, Date.now(), recipient.transactionHash);
+            })
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
     }
-    
-    const erc20Contract = new web3.eth.Contract(abi, recipient.contractAddress);
-    erc20Contract.methods.name().call().then(function(name) {
-      erc20Contract.methods.symbol().call((error, symbol) => {
-        saveNewContract(name + '-' + symbol || '', type, balance, gas, gasUsed, gasPrice, transactionFee, Date.now(), recipient.transactionHash);
-      })
-    });
-  });
+  }
 });
